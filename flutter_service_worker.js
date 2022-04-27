07fdad88791e2be11a7b1151a78a1e5869490a6a
seanwestfall <sean.westfall@gmail.com>
2022-04-27 06:01:16 -0700

    fixed the http error

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -4,9 +4,9 @@ const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 const RESOURCES = {
   "version.json": "3fffa3bcabe3144f70eade5d2eed903d",
-"index.html": "fffd093d3ae615ee97330a785681eb58",
-"/": "fffd093d3ae615ee97330a785681eb58",
-"main.dart.js": "b0e601a48b9e392cfdb11aa10fef867a",
+"index.html": "464cc278249c37038b3fc7eac640a567",
+"/": "464cc278249c37038b3fc7eac640a567",
+"main.dart.js": "d0f0ac828c7c90e6496b7db31885d3cc",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
