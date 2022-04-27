cb30d6e8c75f35fb04b5980aa393bd564805e7d0
seanwestfall <sean.westfall@gmail.com>
2022-04-27 05:57:46 -0700

    update the prod site

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -3,21 +3,27 @@ const MANIFEST = 'flutter-app-manifest';
 const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 const RESOURCES = {
-  "version.json": "071de4c390fae425254d7e540919d4dd",
-"index.html": "18d5fcb4a4a177b398a64bd92b734159",
-"/": "18d5fcb4a4a177b398a64bd92b734159",
-"main.dart.js": "10203b1bfea7ebb438c4eb067b8b60a8",
+  "version.json": "3fffa3bcabe3144f70eade5d2eed903d",
+"index.html": "fffd093d3ae615ee97330a785681eb58",
+"/": "fffd093d3ae615ee97330a785681eb58",
+"main.dart.js": "b0e601a48b9e392cfdb11aa10fef867a",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
 "icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "manifest.json": "db0bc5d4f35d751d4f3903a050c3c07e",
-"assets/AssetManifest.json": "2efbb41d7877d10aac9d091f58ccd7b9",
-"assets/NOTICES": "a5ac28eac48ea57e1d2748f4a143cca0",
+"assets/AssetManifest.json": "c2b4a3d8fb8b7f790e99d1c7dbf4b938",
+"assets/NOTICES": "c93c7b4a6e7707ca972e477db30e85d0",
 "assets/FontManifest.json": "dc3d03800ccca4601324923c0b1d6d57",
 "assets/packages/cupertino_icons/assets/CupertinoIcons.ttf": "6d342eb68f170c97609e9da345464e5e",
-"assets/fonts/MaterialIcons-Regular.otf": "4e6447691c9509f7acdbf8a931a85ca1"
+"assets/fonts/MaterialIcons-Regular.otf": "7e7a6cccddf6d7b20012a548461d5d81",
+"assets/data/pokemon_details_data.json": "7c4ab45e93a1fb55a40b010867c8640c",
+"assets/data/pokemon.json": "8d0b32d9659b31a2a9b7c00ca37114e6",
+"canvaskit/canvaskit.js": "c2b4e5f3d7a3d82aed024e7249a78487",
+"canvaskit/profiling/canvaskit.js": "ae2949af4efc61d28a4a80fffa1db900",
+"canvaskit/profiling/canvaskit.wasm": "95e736ab31147d1b2c7b25f11d4c32cd",
+"canvaskit/canvaskit.wasm": "4b83d89d9fecbea8ca46f2f760c5a9ba"
 };
 
 // The application shell files that are downloaded before a service worker can
